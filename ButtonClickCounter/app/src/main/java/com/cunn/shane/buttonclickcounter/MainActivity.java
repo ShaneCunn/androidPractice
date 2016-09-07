2ff7ab4752352dcba48af106027dbbca7505c0b3
Shane Cunningham <shanecunningham@live.ie>
2016-09-07 13:19:59 +0100

    added click counter for button and cleared text

--- ButtonClickCounter/app/src/main/java/com/cunn/shane/buttonclickcounter/MainActivity.java
+++ ButtonClickCounter/app/src/main/java/com/cunn/shane/buttonclickcounter/MainActivity.java
@@ -1,13 +1,38 @@
 package com.cunn.shane.buttonclickcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private Button button;
+    private TextView textView;
+    private int numTimesClicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("");
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                numTimesClicked = numTimesClicked + 1;
+                String result = "\n The Button got clicked " + numTimesClicked + " time";
+                textView.append(result);
+            }
+        };
+
+        button.setOnClickListener(ourOnClickListener);
     }
 }
