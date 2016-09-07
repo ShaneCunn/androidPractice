d6171ebfb6283a1dc430f6314806c45924f9806a
Shane Cunningham <shanecunningham@live.ie>
2016-09-07 18:15:17 +0100

    added logging to code

--- ButtonClickCounter/app/src/main/java/com/cunn/shane/buttonclickcounter/MainActivity.java
+++ ButtonClickCounter/app/src/main/java/com/cunn/shane/buttonclickcounter/MainActivity.java
@@ -3,6 +3,7 @@ package com.cunn.shane.buttonclickcounter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,17 +12,17 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private EditText userInput;
-
     private TextView textView;
-    // private int numTimesClicked = 0;
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         userInput = (EditText) findViewById(R.id.editText);
-        userInput.setText("");  //clears the EditText of text, replaces with a empty string
 
         Button button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
@@ -34,9 +35,69 @@ public class MainActivity extends AppCompatActivity {
                 String result = userInput.getText().toString();
                 result = result + "\n";
                 textView.append(result);
+                userInput.setText("");  //clears the EditText of text, replaces with a empty string
+
             }
         };
 
         button.setOnClickListener(ourOnClickListener);
+        Log.d(TAG, "onCreate: out");
     }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: in");
+        super.onStart();
+        Log.d(TAG, "onStart: out");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: in");
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: out");
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart: in");
+        super.onRestart();
+        Log.d(TAG, "onRestart: out");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState: out");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: in");
+        super.onStop();
+        Log.d(TAG, "onStop: out");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: in");
+        super.onPause();
+        Log.d(TAG, "onPause: out");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: in");
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: out");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onStart: in");
+        super.onResume();
+        Log.d(TAG, "onStart: out");
+    }
+
 }
